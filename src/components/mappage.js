cca5fc0474185b5b1e38bf02267053f3e378792e
Shivam <ss056989@gmail.com>
2021-10-21 04:00:53 +0530

    useless commit

---
+++ src/components/mappage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './mappage.css';
+
+export const Mappage = () => {
+  return (
+    <div>
+      <img className="bg-map-img" src="./images/map-imp.png" alt="" />
+    </div>
+  )
+}
+
+
